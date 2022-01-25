14d27e267c7ca73e95f3a12b17526acdbb543021
jafarpagerjaya <jafarpager@gmail.com>
2022-01-25 18:00:34 +0700

    compoer update pertama kali

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -2,23 +2,14 @@
     'root' => array(
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'type' => 'library',
+        'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '634a8d7c3b4658882ca5bc6c681ca10e0ffbf3b4',
-        'name' => '__root__',
+        'reference' => '6ef254b110c7c048b2b6c579aa694e11a2f889b4',
+        'name' => 'rahasia/belum.skom.id',
         'dev' => true,
     ),
     'versions' => array(
-        '__root__' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'reference' => '634a8d7c3b4658882ca5bc6c681ca10e0ffbf3b4',
-            'dev_requirement' => false,
-        ),
         'foroco/php-browser-detection' => array(
             'pretty_version' => 'v2.3',
             'version' => '2.3.0.0',
@@ -53,6 +44,15 @@
             'install_path' => __DIR__ . '/../phpmailer/phpmailer',
             'aliases' => array(),
             'reference' => 'baeb7cde6b60b1286912690ab0693c7789a31e71',
+            'dev_requirement' => false,
+        ),
+        'rahasia/belum.skom.id' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6ef254b110c7c048b2b6c579aa694e11a2f889b4',
             'dev_requirement' => false,
         ),
         'symfony/polyfill-mbstring' => array(
